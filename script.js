db9e55c9b8e0bd7bfc64961a392a88c80a97248f
David McDonald <26583259+davmmcdonald@users.noreply.github.com>
2022-11-22 18:49:46 -0600

    Created UI for RPS game

--- script.js
+++ script.js
@@ -1,63 +1,105 @@
-// Here we define the score variables for each player and the computer choice options
-let playerScore=0;
-let computerScore=0;
+// Game variables
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
+let ties = 0;
 const computerOptions = ['rock','paper','scissors'];
 
-// This function collects input from the user, randomly calculates the computer's choice, and determines which wins
-function playRound() {
-    let playerMove = prompt("Enter ROCK, PAPER, OR SCISSORS to play. Good luck!").toLowerCase();
-    let computerMove = computerOptions[Math.floor(Math.random()*3)];
-    console.log("Player Move: "+playerMove);
-    console.log("Computer Move: "+computerMove);
-    
-    if (playerMove === computerMove) {
-        alert("It's a tie!");
-    } 
-    else if (playerMove == 'rock') {
-        if (computerMove == 'scissors') {
-            alert("You won! "+playerMove.toUpperCase()+" beats "+computerMove.toUpperCase()+"!");
-            playerScore++;
-        } 
-        else {
-            alert("You lost! "+computerMove.toUpperCase()+" beats "+playerMove.toUpperCase()+"!");
-            computerScore++;
+// Selected element variables
+const prompts = document.querySelectorAll('.highlight');
+const input = document.querySelector('.input');
+const info = document.querySelector('.info');
+
+// Checks command entered against allowed commands
+function checkCommand(cmd) {
+    console.log(cmd);
+    if (cmd === 'play rock' || cmd === 'play paper' || cmd == 'play scissors') {
+        if (round < 5) {
+            playRound(cmd.replace('play ',''));
+        } else {
+            info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>The game is over. Enter the command <span class="highlight">restart</span> to play again!`);
         }
-    }
-    else if (playerMove == 'paper') {
-        if (computerMove == 'rock') {
-            alert("You won! "+playerMove.toUpperCase()+" beats "+computerMove.toUpperCase()+"!");
-            playerScore++;
-        } 
-        else {
-            alert("You lost! "+computerMove.toUpperCase()+" beats "+playerMove.toUpperCase()+"!");
-            computerScore++;
-        }
-    }
-    else if (playerMove == 'scissors') {
-        if (computerMove == 'paper') {
-            alert("You won! "+playerMove.toUpperCase()+" beats "+computerMove.toUpperCase()+"!");
-            playerScore++;
-        } 
-        else {
-            alert("You lost! "+computerMove.toUpperCase()+" beats "+playerMove.toUpperCase()+"!");
-            computerScore++;
+        
+    } else {
+        if (cmd === 'get score') {
+            info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>Your score is `+playerScore+` and the computer's score is `+computerScore+'.');
+        } else {
+            if (cmd === 'restart') {
+                location.reload();
+            } else {
+                if (cmd === 'get round') {
+                    info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>You've played `+round+' round(s). ');
+                } else {
+                    console.log('error');
+                    returnError(cmd);
+                }
+            } 
         }
     }
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        console.log("Player Score: "+playerScore);
-        console.log("Computer Score: "+computerScore);
-        alert("Your score is "+playerScore+" and the computer's score is "+computerScore);
+// Runs the round, comparing the players move to the computer's
+function playRound(action) {
+    let computerMove = computerOptions[Math.floor(Math.random()*3)];
+    if (action === computerMove) {
+        info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>It's a tie!`);
+        ties += 1;
+    } else {
+        if (action === "rock" && computerMove !== "paper") {
+            info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>Your `+action+` defeats the computer's `+computerMove+'!');
+            playerScore += 1;
+            round += 1;
+        } else {
+            if (action === "paper" && computerMove !== "scissors") {
+                info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>Your `+action+` defeats the computer's `+computerMove+'!');
+                playerScore += 1;
+                round += 1;
+            } else {
+                if (action === "scissors" && computerMove !== "rock") {
+                    info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>Your `+action+` defeats the computer's `+computerMove+'!');
+                    playerScore += 1;
+                    round += 1;
+                } else {
+                    info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>Your `+action+` loses to the computer's `+computerMove+'!');
+                    computerScore += 1;
+                    round += 1;
+                }
+            }
+        }
     }
-    if (playerScore > computerScore) {
-        let difference = Math.abs(playerScore - computerScore);
-        alert("After 5 brutal rounds, you won by "+difference+" points. Congrats!");
+    if (round == 5) {
+        if (playerScore > computerScore) {
+            info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>After five brutal rounds, you beat the computer by `+(playerScore-computerScore)+' point(s)!');
+        } else {
+            info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>After five brutal rounds, the computer beat you by `+(computerScore-playerScore)+' point(s)!');
+        }
+    } 
+    if (ties == 5) {
+        info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>Damn that's a lot of ties!`);
     }
-    else {
-        let difference = Math.abs(playerScore - computerScore);
-        alert("After 5 brutal rounds, you lost by "+difference+" points. Congrats!");
+}
+
+// Returns an error message for invalid commands
+function returnError(str) {
+    info.insertAdjacentHTML('beforeend',`<br><span style='color: yellow;'>$ </span>Command not valid: `+str);
+}
+
+// Processes inputted user commands when the enter key is pressed
+window.addEventListener('keydown',e => {
+    if (e.keyCode === 13) {
+        checkCommand(input.value.toLowerCase());
+        input.value = '';
+        scrollToBottom();
     }
+});
+
+// Makes highlighted code in instructions clickable and automatically adds them to the input field
+prompts.forEach(prompt => prompt.addEventListener('click',e => {
+    input.value = e.target.innerText;
+    document.querySelector('.input').focus();
+}));
+
+// Scroll to bottom of page
+function scrollToBottom() {
+    window.scrollTo(0,document.body.scrollHeight);
 }
